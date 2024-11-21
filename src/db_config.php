ae06768f7623caff0b12611224c40e0210d81e8b
facucarranza <facundo.carranza.222@gmail.com>
2024-11-21 17:40:38 +0000

    Hemos cambiado la version de la imagen de nuestro contenedor my sql ya ademas hemos anadido a nuestro db_config.php un script para crear automaticamente nuestra tabla users en caso de que la misma no este creada

--- src/db_config.php
+++ src/db_config.php
@@ -7,6 +7,13 @@ $password = 'admpassword';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE )";
+    
+    $pdo->exec($sql);
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
     $pdo = null;
